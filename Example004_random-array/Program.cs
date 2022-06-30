fdb2fb31da461de621a54397a1a244b99152147d
Max Sharan <qshark55@gmail.com>
2022-07-01 00:34:34 +0300

    Example 004 random array with find index

--- Example004_random-array/Program.cs
+++ Example004_random-array/Program.cs
@@ -6,7 +6,7 @@
     {
         collection[index] = new Random().Next(1, 33);
         index++;
-    } 
+    }
 }
 
 void PrintArray(int[] col)
@@ -19,7 +19,38 @@ void PrintArray(int[] col)
         position++;
     }
 }
-int[] array = new int[9];
+
+void IndexOf(int[] collection, int find)
+{
+    int index = 0;
+    int length = collection.Length;
+    int position = 0;
+
+    while(index < length)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            Console.WriteLine("Индекс совпадающего элемента = " + position);
+            break;
+        }
+        index++;
+        if(index == length) 
+        {
+            Console.WriteLine("Совпадений не найдено!");
+        }
+    }
+    // return position; // это если мы решим сделать метод с возвратом результата.
+}
+
+int[] array = new int[15];
 
 FillArray(array);
 PrintArray(array);
+
+Console.WriteLine();
+
+// int pos = IndexOf(array, 155); // это при использовании метода с возвратом результата.
+IndexOf(array, 15);
+// Console.WriteLine("Индекс совпадающего элемента = " + pos);
+                                    // это при использовании метода с возвратом результата.
