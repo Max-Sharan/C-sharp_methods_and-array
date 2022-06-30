0c1bfbd7bc2c4c5ad2443935ebab9908e6e6305c
Max Sharan <qshark55@gmail.com>
2022-06-30 20:10:13 +0300

    Example 003 find index element with compare the number

---
+++ Example003_array-find-index/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 10, 157, 31, 45, 57, 384, 19, 28, 384, 122 };
+
+int find = 122;
+int index = 0;
+int n = array.Length;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine("Индекс совпадающего элемента = " + index);
+        break;
+    }
+    index++;
+    if(index == n) 
+    {
+        Console.WriteLine("Совпадений не найдено!");
+    }
+}
