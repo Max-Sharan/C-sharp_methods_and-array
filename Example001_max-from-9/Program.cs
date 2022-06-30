fcccb6f8e7c7bcdf4a360b48d7180d2a11123c3f
Max Sharan <qshark55@gmail.com>
2022-06-30 16:58:06 +0300

    replace with metod inside metod

--- Example001_max-from-9/Program.cs
+++ Example001_max-from-9/Program.cs
@@ -6,7 +6,7 @@
     return result;
 }
 
-int a1 = 1500;
+int a1 = 15000;
 int a2 = 42;
 int a3 = 13;
 int b1 = 25;
@@ -16,12 +16,17 @@ int c1 = 3737;
 int c2 = 412;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
 
-Console.WriteLine("Победитель первой 3-ки = " + max1);
-Console.WriteLine("Победитель второй 3-ки = " + max2);
-Console.WriteLine("Победитель третьей 3-ки = " + max3);
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));  // метод внутри метода, можно и в одну строку!
+
+Console.WriteLine("Победитель первой 3-ки = " + Max(a1, b1, c1));
+Console.WriteLine("Победитель второй 3-ки = " + Max(a2, b2, c2));
+Console.WriteLine("Победитель третьей 3-ки = " + Max(a3, b3, c3));
 Console.WriteLine("Максимальное число среди всех = " + max);
