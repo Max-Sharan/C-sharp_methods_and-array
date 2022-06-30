e366502720f97b16ab97f8b6626459e7e99df94b
Max Sharan <qshark55@gmail.com>
2022-06-30 20:40:46 +0300

    Example 004 print random array 9 elements

---
+++ Example004_random-array/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 33);
+        index++;
+    } 
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+int[] array = new int[9];
+
+FillArray(array);
+PrintArray(array);
