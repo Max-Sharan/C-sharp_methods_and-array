fbb57fe754bef3d429477d19eeacce551dce5bcf
Max Sharan <qshark55@gmail.com>
2022-06-30 16:17:13 +0300

    Example 001 max from 9 classic style

---
+++ Example001_max-from-9/Program.cs
@@ -0,0 +1,24 @@
+﻿int a1 = 1500;
+int a2 = 42;
+int a3 = 13;
+int b1 = 25;
+int b2 = 32;
+int b3 = 23;
+int c1 = 3737;
+int c2 = 412;
+int c3 = 33;
+
+int max = a1;
+
+if (b1 > max) max = b1;
+if (b2 > max) max = b2;
+if (b3 > max) max = b3;
+
+if (c1 > max) max = c1;
+if (c2 > max) max = c2;
+if (c3 > max) max = c3;
+
+if (a2 > max) max = a2;
+if (a3 > max) max = a3;
+
+Console.WriteLine("Максимальное число среди всех = " + max);
