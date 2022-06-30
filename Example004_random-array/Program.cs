a6026df2fe43b2b1af0209db37043d1c77ba0e54
Max Sharan <qshark55@gmail.com>
2022-07-01 00:54:34 +0300

    add random find, formating results and clear console

--- Example004_random-array/Program.cs
+++ Example004_random-array/Program.cs
@@ -1,10 +1,12 @@
-﻿void FillArray(int[] collection)
+﻿Console.Clear();
+
+void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 33);
+        collection[index] = new Random().Next(1, 55);
         index++;
     }
 }
@@ -13,6 +15,7 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
+    Console.WriteLine("Сгенерированный массив:");
     while (position < count)
     {
         Console.Write(col[position] + " ");
@@ -25,13 +28,14 @@ void IndexOf(int[] collection, int find)
     int index = 0;
     int length = collection.Length;
     int position = 0;
-
+    
     while(index < length)
     {
         if (collection[index] == find)
         {
             position = index;
-            Console.WriteLine("Индекс совпадающего элемента = " + position);
+            
+            Console.WriteLine("Индекс элемента совпадающего с числом = " + position);
             break;
         }
         index++;
@@ -43,14 +47,14 @@ void IndexOf(int[] collection, int find)
     // return position; // это если мы решим сделать метод с возвратом результата.
 }
 
-int[] array = new int[15];
+int[] array = new int[27];
+int find = new Random().Next(1, 55);
 
 FillArray(array);
 PrintArray(array);
-
 Console.WriteLine();
-
+Console.WriteLine("Искомое число среди элементов массива = " + find);
 // int pos = IndexOf(array, 155); // это при использовании метода с возвратом результата.
-IndexOf(array, 15);
+IndexOf(array, find);
 // Console.WriteLine("Индекс совпадающего элемента = " + pos);
                                     // это при использовании метода с возвратом результата.
