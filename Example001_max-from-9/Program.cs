e1eba898a502d48d40e2c761fb0ca4cd57b0669e
Max Sharan <qshark55@gmail.com>
2022-06-30 16:48:42 +0300

    reused with metods

--- Example001_max-from-9/Program.cs
+++ Example001_max-from-9/Program.cs
@@ -1,24 +1,27 @@
-﻿int a1 = 1500;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 1500;
 int a2 = 42;
 int a3 = 13;
 int b1 = 25;
 int b2 = 32;
-int b3 = 23;
+int b3 = 7623;
 int c1 = 3737;
 int c2 = 412;
 int c3 = 33;
 
-int max = a1;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
-if (b1 > max) max = b1;
-if (b2 > max) max = b2;
-if (b3 > max) max = b3;
-
-if (c1 > max) max = c1;
-if (c2 > max) max = c2;
-if (c3 > max) max = c3;
-
-if (a2 > max) max = a2;
-if (a3 > max) max = a3;
-
+Console.WriteLine("Победитель первой 3-ки = " + max1);
+Console.WriteLine("Победитель второй 3-ки = " + max2);
+Console.WriteLine("Победитель третьей 3-ки = " + max3);
 Console.WriteLine("Максимальное число среди всех = " + max);
