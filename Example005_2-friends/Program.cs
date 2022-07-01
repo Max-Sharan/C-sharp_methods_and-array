92376897dae59a2f654b84b565a39178fcf4752d
Max Sharan <qshark55@gmail.com>
2022-07-01 20:19:00 +0300

    Example 005 2 friends and dog runs

---
+++ Example005_2-friends/Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.Clear();
+
+int firstFriendSpeed = 3,
+    secondFriendSpeed = 2,
+    dogSpeed = 7,
+    count = 0;
+
+double distance = 25000,   
+       time = 0;
+bool firstFriendDirection = false;
+
+while (distance > 2)
+{
+    if (firstFriendDirection)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        firstFriendDirection = true;
+    }
+    else
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        firstFriendDirection = false;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine("Собака пробежит от одного друга к другому " + count + " раз");
